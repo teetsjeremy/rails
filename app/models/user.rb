46dccad7f20eba7531ca2c3a591fe84f77af966e
Jeremy Teets <jeremy@Jeremys-Air.attlocal.net>
2020-07-14 21:43:11 -0700

    added username to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,29 @@ class User < ApplicationRecord
   has_one_attached :name
   after_commit :add_default_avatar, on: %i[create update]
 
+  attr_writer :login
+  validate :validate_username
+
+  def validate_username
+    errors.add(:username, :invalid) if User.where(email: username).exists?
+  end
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value', { :value => login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
   def avatar_thumbnail
     if avatar.attached?
-    avatar.variant(resize: "150x150").processed
+    avatar.variant(resize: '150x150').processed
     else
       '/default_profile.jpg'
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_030721) do
+ActiveRecord::Schema.define(version: 2020_07_15_031622) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,9 +64,11 @@ ActiveRecord::Schema.define(version: 2020_07_11_030721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "username"
     t.index "\"confirmation_token\"", name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "views", force: :cascade do |t|
